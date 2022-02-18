3325e21dbf4d79cf44cf251f85d4a05e2986e471
nils2obi <nils.obi10@gmail.com>
2022-02-18 14:48:10 +0100

    Sttuspage verbessert

--- pages/status.js
+++ pages/status.js
@@ -19,7 +19,7 @@ async function loadData() {
 };
 
 
-function loadmaxTemp(temp1, temp2, temp3) {
+function loadmax(temp1, temp2, temp3) {
   if (temp1 >= temp2) {
     if (temp1 >= temp3) {
       return temp1
@@ -37,7 +37,7 @@ function loadmaxTemp(temp1, temp2, temp3) {
   };
 };
 
-function loadminTemp(temp1, temp2, temp3) {
+function loadmin(temp1, temp2, temp3) {
   if (temp1 <= temp2) {
     if (temp1 <= temp3) {
       return temp1
@@ -57,12 +57,16 @@ function loadminTemp(temp1, temp2, temp3) {
 
 function loadThermometer(temp1, temp2, temp3) {
 
+  document.querySelector("#Temperatur1").style.left = "5.25cm";
+  document.querySelector("#Temperatur2").style.left = "5.25cm";
+  document.querySelector("#Temperatur3").style.left = "5.25cm";
+
   temp1 = temp1.toFixed(1);
   temp2 = temp2.toFixed(1);
   temp3 = temp3.toFixed(1);
 
-  var tempmax = loadmaxTemp(temp1, temp2, temp3);
-  var tempmin = loadminTemp(temp1, temp2, temp3);
+  var tempmax = loadmax(temp1, temp2, temp3);
+  var tempmin = loadmin(temp1, temp2, temp3);
 
   if (tempmax == temp1 && tempmin == temp2 || tempmax == temp2 && tempmin == temp1) {
     document.querySelector("#Temperatur3").style.left = "0.25cm";
@@ -78,9 +82,9 @@ function loadThermometer(temp1, temp2, temp3) {
   document.querySelector("#untereGrenze").innerHTML = parseFloat(tempmin) - parseFloat(abweichung);
   document.querySelector("#obereGrenze").innerHTML = (parseFloat(tempmax) + parseFloat(abweichung)).toFixed(1);
 
-  document.querySelector("#Temperatur1").innerHTML = temp1;
-  document.querySelector("#Temperatur2").innerHTML = temp2;
-  document.querySelector("#Temperatur3").innerHTML = temp3;
+  document.querySelector("#Temperatur1").innerHTML = temp1 + " °C";
+  document.querySelector("#Temperatur2").innerHTML = temp2 + " °C";
+  document.querySelector("#Temperatur3").innerHTML = temp3 + " °C";
 
   tempspann = tempspann + 2 * abweichung;
 
@@ -93,12 +97,53 @@ function loadThermometer(temp1, temp2, temp3) {
   document.querySelector("#Strich3").style.top = 0.6 + (5.5 / tempspann * (parseFloat(tempmax) + parseFloat(abweichung) - parseFloat(temp3))) + "cm";
 
   document.querySelector(".Thermometercase").style.display = "block";
-
 };
 
-function openTemperature(temp){
-  console.log(" Temperatur: " + temp);
+function loadHydrometer(humd1, humd2, humd3) {
+
+  document.querySelector("#Luftfeuchtigkeit1").style.left = "5.25cm";
+  document.querySelector("#Luftfeuchtigkeit2").style.left = "5.25cm";
+  document.querySelector("#Luftfeuchtigkeit3").style.left = "5.25cm";
+
+  humd1 = humd1.toFixed(1);
+  humd2 = humd2.toFixed(1);
+  humd3 = humd3.toFixed(1);
+
+  var humdmax = loadmax(humd1, humd2, humd3);
+  var humdmin = loadmin(humd1, humd2, humd3);
+
+  if (humdmax == humd1 && humdmin == humd2 || humdmax == humd2 && humdmin == humd1) {
+    document.querySelector("#Luftfeuchtigkeit3").style.left = "0.25cm";
+  } else if (humdmax == humd2 && humdmin == humd3 || humdmax == humd3 && humdmin == humd2) {
+    document.querySelector("#Luftfeuchtigkeit1").style.left = "0.25cm";
+  } else if (humdmax == humd1 && humdmin == humd3 || humdmax == humd3 && humdmin == humd1) {
+    document.querySelector("#Luftfeuchtigkeit2").style.left = "0.25cm";
+  };
+
+  var humdspann = humdmax - humdmin;
+  var abweichung = Math.round(humdspann) / 10;
+  humdspann = Math.round(humdspann * 10) / 10;
+  document.querySelector("#untereGrenzehumd").innerHTML = parseFloat(humdmin) - parseFloat(abweichung);
+  document.querySelector("#obereGrenzehumd").innerHTML = (parseFloat(humdmax) + parseFloat(abweichung)).toFixed(1);
+
+  document.querySelector("#Luftfeuchtigkeit1").innerHTML = humd1 + " %";
+  document.querySelector("#Luftfeuchtigkeit2").innerHTML = humd2 + " %";
+  document.querySelector("#Luftfeuchtigkeit3").innerHTML = humd3 + " %";
+
+  humdspann = humdspann + 2 * abweichung;
+
+  document.querySelector("#Luftfeuchtigkeit1").style.top = 0.25 + (5.5 / humdspann * (parseFloat(humdmax) + parseFloat(abweichung) - parseFloat(humd1))) + "cm";
+  document.querySelector("#Luftfeuchtigkeit2").style.top = 0.25 + (5.5 / humdspann * (parseFloat(humdmax) + parseFloat(abweichung) - parseFloat(humd2))) + "cm";
+  document.querySelector("#Luftfeuchtigkeit3").style.top = 0.25 + (5.5 / humdspann * (parseFloat(humdmax) + parseFloat(abweichung) - parseFloat(humd3))) + "cm";
+
+  document.querySelector("#Strich1humd").style.top = 0.6 + (5.5 / humdspann * (parseFloat(humdmax) + parseFloat(abweichung) - parseFloat(humd1))) + "cm";
+  document.querySelector("#Strich2humd").style.top = 0.6 + (5.5 / humdspann * (parseFloat(humdmax) + parseFloat(abweichung) - parseFloat(humd2))) + "cm";
+  document.querySelector("#Strich3humd").style.top = 0.6 + (5.5 / humdspann * (parseFloat(humdmax) + parseFloat(abweichung) - parseFloat(humd3))) + "cm";
+
+  document.querySelectorAll(".Thermometercase")[1].style.display = "block";
 };
+loadHydrometer(60,62,55);
+loadThermometer(10,20,30);
 
 function loadsite() {
   startload();
